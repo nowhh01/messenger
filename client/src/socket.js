32214b3c5cc4a8017fd2be52ce1482d4951e7704
nowhh01 <39201456+nowhh01@users.noreply.github.com>
2021-07-26 15:14:45 -0400

    add observer on updated-messages

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,6 +4,7 @@ import {
   setNewMessage,
   removeOfflineUser,
   addOnlineUser,
+  replaceMessages,
 } from "./store/conversations";
 
 const socket = io(window.location.origin);
@@ -18,8 +19,13 @@ socket.on("connect", () => {
   socket.on("remove-offline-user", (id) => {
     store.dispatch(removeOfflineUser(id));
   });
+
   socket.on("new-message", (data) => {
     store.dispatch(setNewMessage(data.message, data.sender));
+  });
+
+  socket.on("updated-messages", (messages) => {
+    store.dispatch(replaceMessages(messages));
   });
 });
 
