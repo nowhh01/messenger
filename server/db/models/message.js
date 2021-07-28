63a4f6e8638a9da89515a09ce24c12e0958915ad
nowhh01 <39201456+nowhh01@users.noreply.github.com>
2021-07-28 14:41:37 -0400

    add find func

--- server/db/models/message.js
+++ server/db/models/message.js
@@ -1,19 +1,36 @@
-const Sequelize = require('sequelize');
-const db = require('../db');
+const Sequelize = require("sequelize");
+const db = require("../db");
+const { Op } = require("sequelize");
 
-const Message = db.define('message', {
+const Message = db.define("message", {
   text: {
     type: Sequelize.STRING,
-    allowNull: false,
+    allowNull: false
   },
   senderId: {
     type: Sequelize.INTEGER,
-    allowNull: false,
+    allowNull: false
   },
   isRead: {
     type: Sequelize.BOOLEAN,
-    allowNull: true,
-  },
+    allowNull: true
+  }
 });
+
+Message.findAllUnreadFromOther = async function (userId, conversationId) {
+  const messages = await Message.findAll({
+    where: {
+      conversationId: conversationId,
+      senderId: {
+        [Op.not]: userId
+      },
+      isRead: {
+        [Op.not]: true
+      }
+    }
+  });
+
+  return messages;
+};
 
 module.exports = Message;
